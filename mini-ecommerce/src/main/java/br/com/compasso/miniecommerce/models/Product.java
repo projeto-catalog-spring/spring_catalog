df06a91ca3b53dc8b5784264d1eec732542b6f4b
Marcos Goulart de Oliveira <marcos.goulart@pampa.compasso>
2019-09-04 13:49:59 -0300

    [FIX] Product model was updated and product dto was created.   new file:   mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/controllers/dto/ProductDto.java                                                          modified:   mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/models/Brand.java                                                                        modified:   mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/models/Category.java                                                                     modified:   mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/models/Product.java

--- mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/models/Product.java
+++ mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/models/Product.java
@@ -1,23 +1,23 @@
 package br.com.compasso.miniecommerce.models;
 
 import lombok.AllArgsConstructor;
-import lombok.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @NoArgsConstructor @AllArgsConstructor
 public class Product {
-		@Id @Generated
+		@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 		@Getter private Long id; 
 	    @Getter @Setter private String nome;
 	    @Getter @Setter private String description;
 	    @Getter @Setter private boolean enable;
-	    @Getter @Setter private Long idCategory;
-	    @Getter @Setter private Long idBrand;
-	    @Getter @Setter private Long idPrice;
+	    @Getter @Setter private Category category;
+	    @Getter @Setter private Brand brand;
 }
