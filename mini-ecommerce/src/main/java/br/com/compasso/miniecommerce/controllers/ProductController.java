84a404d6e12ce5636f6d786acb83374b30ab2450
Angel <angel.sp06@gmail.com>
2019-09-04 16:32:08 -0300

    [FEAT] implemention body controller

--- mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/controllers/ProductController.java
+++ mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/controllers/ProductController.java
@@ -1,5 +1,36 @@
 package br.com.compasso.miniecommerce.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+	
+	@Autowired
+	private ProductRepository productrep;
+	
+	@GetMapping("/product")
+	public List <Product> getAllProducts(){
+		return productrep.findAll();
+	}
+	
+	@PostMapping("/product")
+	
+	
+	@PutMapping("/product")
+	
+	
+	@DeleteMapping("/product")
+	
+	
+	
+	
+	
+	
+	
 
 }
