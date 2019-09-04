090ded6f5b340c4d65cff5c96adb4cdcc7b10c3f
Angel <angel.sp06@gmail.com>
2019-09-04 18:27:58 -0300

    [FIX] Building product controller

--- mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/controllers/ProductController.java
+++ mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/controllers/ProductController.java
@@ -2,13 +2,20 @@ package br.com.compasso.miniecommerce.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.compasso.miniecommerce.models.Product;
+import br.com.compasso.miniecommerce.models.dto.ProductDto;
 import br.com.compasso.miniecommerce.repository.ProductRepository;
 
 @RestController
@@ -18,9 +25,26 @@ public class ProductController {
 	@Autowired
 	private ProductRepository productrep;
 	
-	@GetMapping("/products")
+	@GetMapping 
 	public List <Product> getAllProducts(){
 		return productrep.findAll();
+	}
+	
+	@PostMapping
+	public Product set(@Validated @RequestBody ProductDto products) {
+		return productrep.save(products);
+	}
+	
+	@PutMapping("/{id}")
+	public Product insert(ProductDto id) {
+		return productrep.findOne(id);
+	}
+	
+	@DeleteMapping
+	public void delete(ProductDto id) {
+		if(productrep.findOne(id)!=null) {
+			id.isEnable();
+		} 
 	}
 	
 }
@@ -32,4 +56,4 @@ public class ProductController {
 	
 	
 
-}
+
--- mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/repository/ProductRepository.java
+++ mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/repository/ProductRepository.java
@@ -3,7 +3,14 @@ package br.com.compasso.miniecommerce.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.compasso.miniecommerce.models.Product;
+import br.com.compasso.miniecommerce.models.dto.ProductDto;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	Product save(ProductDto products);
+
+	Product findOne(ProductDto id);
+
+
 
 }
