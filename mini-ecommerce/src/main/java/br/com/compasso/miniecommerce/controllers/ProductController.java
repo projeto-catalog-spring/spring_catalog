5c05aa8d2572ae6b0c698fdc28b324b3701924db
Angel <angel.sp06@gmail.com>
2019-09-04 16:41:03 -0300

    [FIX] integrate branchs

--- mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/controllers/ProductController.java
+++ mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/controllers/ProductController.java
@@ -1,10 +1,15 @@
 package br.com.compasso.miniecommerce.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.compasso.miniecommerce.models.Product;
+import br.com.compasso.miniecommerce.repository.ProductRepository;
 
 @RestController
 @RequestMapping("/products")
@@ -13,18 +18,12 @@ public class ProductController {
 	@Autowired
 	private ProductRepository productrep;
 	
-	@GetMapping("/product")
+	@GetMapping("/products")
 	public List <Product> getAllProducts(){
 		return productrep.findAll();
 	}
 	
-	@PostMapping("/product")
-	
-	
-	@PutMapping("/product")
-	
-	
-	@DeleteMapping("/product")
+}
 	
 	
 	
