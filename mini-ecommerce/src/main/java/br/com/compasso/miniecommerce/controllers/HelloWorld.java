cccf3b9e306a521a88ac4ebca921dfa4673e99ce
Rafael Neves Romeu <rafael.romeu@pampa.compasso>
2019-09-04 11:49:48 -0300

    Configuração do banco com JPA

--- mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/controllers/HelloWorld.java
+++ mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/controllers/HelloWorld.java
@@ -1,14 +1,28 @@
 package br.com.compasso.miniecommerce.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.compasso.miniecommerce.models.Customer;
+import br.com.compasso.miniecommerce.models.CustomerRepository;
 
 @RestController
 public class HelloWorld {
 
+	@Autowired
+	private CustomerRepository customer;
+	
 	@GetMapping("/")
 	public String helloWorld()
 	{
+		Customer c = new Customer("Rafael", "Romeu");
+		customer.save(c);
+		
+		Iterable<Customer> c1 = customer.findAll();
+		
+		System.out.println(c1);
+		
 		return "Hello World";
 	}
 }
