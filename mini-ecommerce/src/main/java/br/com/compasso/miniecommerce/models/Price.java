1ddf61bb5f02eebabc41cbd9b62e8e3175248ed4
Marcos Goulart de Oliveira <marcos.goulart@pampa.compasso>
2019-09-04 11:29:13 -0300

    [FEAT] Create  model and repository for Price.                 modified:   mini-ecommerce/pom.xml                                              new file:   mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/models/Price.java                                                                        new file:   mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/repository/PriceRepository.java

---
+++ mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/models/Price.java
@@ -0,0 +1,20 @@
+package br.com.compasso.miniecommerce.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+public class Price {
+ 
+    @Id @GeneratedValue
+    @Getter private Long id; 
+    @Getter @Setter private double price;
+    @Getter @Setter private double salePrice;
+ }
