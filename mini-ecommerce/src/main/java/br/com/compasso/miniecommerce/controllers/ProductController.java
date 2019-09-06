e6d56e5ab23f735b48c1309d79ee64110e672401
Angel <angel.sp06@gmail.com>
2019-09-06 15:52:23 -0300

    [FIX] Refresh branch

--- mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/controllers/ProductController.java
+++ mini-ecommerce/src/main/java/br/com/compasso/miniecommerce/controllers/ProductController.java
@@ -1,10 +1,14 @@
 package br.com.compasso.miniecommerce.controllers;
 
+
 import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
+
+import org.hibernate.validator.internal.util.privilegedactions.NewInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +17,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.compasso.miniecommerce.models.Product;
 import br.com.compasso.miniecommerce.models.dto.ProductDto;
 import br.com.compasso.miniecommerce.repository.ProductRepository;
+import io.swagger.models.Response;
+import org.modelmapper.ModelMapper;
 
 @RestController
 @RequestMapping("/products")
@@ -25,22 +35,31 @@ public class ProductController {
 	
 	@Autowired
 	private ProductRepository productrep;
-	
+
+
 	@GetMapping 
 	public Iterable<Product> getAllProducts(){
 		return productrep.findAll();
 	}
 
-	
+
+
 	@PostMapping
-	public Product set(@Validated Product products) {
-		return productrep.save(products);
+	public String set(@Validated @RequestBody ProductDto products) {
+		
+		ModelMapper modelMapper = new ModelMapper();
+//		modelMapper.map(, Product.class);
+		modelMapper.typeMap(product, Product.class);
+		productrep.save(products);
+		return "teste";
 	} 
-	
-	
+
+		
+
 	@PutMapping
-	public Product insert(@RequestBody Product products) {
-		return productrep.save(products);
+	public String insert(Product products) {
+			productrep.save(products);
+		 return "Save";
 	}
 	
 	
