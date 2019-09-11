3843f4766354930c66ebfcdc34e9cc44e15da7eb
Marcos Goulart de Oliveira <marcos.goulart@pampa.compasso>
2019-09-11 12:06:46 -0300

    [FEAT] SKU Service unit testers have been added.

--- mini-ecommerce/src/test/java/br/com/compasso/miniecommerce/models/PriceTest.java
+++ mini-ecommerce/src/test/java/br/com/compasso/miniecommerce/models/PriceTest.java
@@ -9,28 +9,43 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import br.com.compasso.miniecommerce.models.Price;
-
 @RunWith(MockitoJUnitRunner.class)
 public class PriceTest {
-	
+
 	@Mock
 	private Price mockedPrice;
-	
+
 	@Before
 	public void setup() {
+
 	}
-	
+
 	@Test
-	public void priceConstructorTest() {
-		mockedPrice = new Price(new Long(4), 3.2, 3.8);
+	public void constructorTest() {
+
+		mockedPrice = new Price(new Long(4), 2.45, 6.85);
+		assertEquals(new Long(4), mockedPrice.getId());
+		assertEquals("O preço está errado.", 2.45, mockedPrice.getPrice(), 0);
+		assertEquals("O preço de venda está errado.", 6.85, mockedPrice.getSalePrice(), 0);
+
+	}
+
+	@Test
+	public void priceGetIdTest() {
+		when(mockedPrice.getId()).thenReturn(new Long(4));
 		assertEquals(new Long(4), mockedPrice.getId());
 	}
-	
+
 	@Test
-    public void priceGetIdTest() {
-		when(mockedPrice.getId()).thenReturn(new Long(4));
-        assertEquals(new Long(4), mockedPrice.getId());
-    }
-	
+	public void priceGetPriceTest() {
+		when(mockedPrice.getPrice()).thenReturn(2.45);
+		assertEquals("O preço está errado.", 2.45, mockedPrice.getPrice(), 0);
+	}
+
+	@Test
+	public void priceGetSalePriceTest() {
+		when(mockedPrice.getSalePrice()).thenReturn(6.85);
+		assertEquals("O preço de venda está errado.", 6.85, mockedPrice.getSalePrice(), 0);
+	}
+
 }
